31c99cdf74f5848bb48886561e4b521a876ee456
League Student <league@administrators-iMac-3.local>
2024-01-29 16:58:09 -0800

    racing once more
    in theory race works, but winner message appears too many times. threads are very useful

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,56 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
+	public static void main(String[] args) {
+		Robot ringOne = new Robot(100,400);
+		Robot ringTwo= new Robot(350,600);
+		Robot ringThree = new Robot(600,400);
+		Robot ringFour = new Robot(850,600);
+		Robot ringFive = new Robot(1100,400);
+		
+		ringOne.setSpeed(10);
+		ringOne.setPenColor(Color.BLUE);
+		ringTwo.setSpeed(10);
+		ringTwo.setPenColor(Color.YELLOW);
+		ringThree.setSpeed(10);
+		ringThree.setPenColor(Color.BLACK);
+		ringFour.setSpeed(10);
+		ringFour.setPenColor(Color.GREEN);
+		ringFive.setSpeed(10);
+		ringFive.setPenColor(Color.RED);
+		
+		
+		Thread r1 = new Thread(()->drawCircle(ringOne));
+		Thread r2 = new Thread(()->drawCircle(ringTwo));
+		Thread r3 = new Thread(()->drawCircle(ringThree));
+		Thread r4 = new Thread(()->drawCircle(ringFour));
+		Thread r5 = new Thread(()->drawCircle(ringFive));
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		
+		
+	}
+	public static void drawCircle(Robot rob) {
+		rob.setPenWidth(5);
+		rob.penDown();
+		int angle = 0;
+		for (int i = 0; i < 360; i++) {
+			int update = angle +1;
+			rob.setAngle(update);
+			rob.move(3);
+			angle = update;
+		}
+		rob.penUp();
+		rob.hide();
+	}
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,62 @@
 package _02_Advanced_Robot_Race;
 
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
-	// This time, use threads to make all of the robots go at the same time.
+	public static boolean race = true;
+    // 1. make a main method
+public static void main(String[] args) {
+Robot[]robots = new Robot[10];
+
+for (int i = 0; i < robots.length; i++) {
+	robots[i] = new Robot((i*100)+100,700);
+	robots[i].setPenColor(Color.BLACK);
+	robots[i].setSpeed(10);
+}
+Thread t1 = new Thread(()->race(robots[0]));
+Thread t2 = new Thread(()->race(robots[1]));
+Thread t3 = new Thread(()->race(robots[2]));
+Thread t4 = new Thread(()->race(robots[3]));
+Thread t5 = new Thread(()->race(robots[4]));
+Thread t6 = new Thread(()->race(robots[5]));
+Thread t7 = new Thread(()->race(robots[6]));
+Thread t8 = new Thread(()->race(robots[7]));
+Thread t9 = new Thread(()->race(robots[8]));
+Thread t10 = new Thread(()->race(robots[9]));
+t1.start();
+t2.start();
+t3.start();
+t4.start();
+t5.start();
+t6.start();
+t7.start();
+t8.start();
+t9.start();
+t10.start();
+
+
+        
+}
+public static void race(Robot rob) {
+while(rob.getY()>50) {
+		int dist = new Random().nextInt(50);
+		rob.move(dist);
+	}
+if (race==true) {
+	winner(rob);
+	race=false;
+}
+
+
+}
+public static void winner (Robot rob) {
+	JOptionPane.showMessageDialog(null, "Winner!");
+	race=false;
+
+}
 }
